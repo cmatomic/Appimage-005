7e43bc0321d1bd73040f269277a9e61062176157
cmatomic <33186553+cmatomic@users.noreply.github.com>
2019-10-01 02:31:07 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # VLC version import from snap ubuntu 
 # Script tested in Ubunu xenial 16.04 and Debian 10 (buster)
 # after running this script then you can go to the ~out\ folder and click on the generated Vlc appimage
-# 27-09-2019 "DDMMYYY"
+# 1-10-2019 "DDMMYYY"
 mkdir VLCsnapBuild
 chmod 755 VLCsnapBuild
 cd VLCsnapBuild
@@ -20,22 +20,34 @@ $here ./pkg2appimage SnapUnion.yml
 $here ./out/Snap-*.AppImage download vlc
 $here ./out/squashfs-tools*.AppImage vlc_*.snap
 mv squashfs-root/ VlcPlayerSnap-x86_64.AppDir/
-mv Vlc*/usr/lib/x86_64-linux-gnu/libgtk-3.so.0  VlcPlayerSnap-x86_64.AppDir/usr/lib/x86_64-linux-gnu/libgtk-3.so.0.back
-
-wget  https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/AppRun -P Vlc*
+#mv Vlc*/usr/lib/x86_64-linux-gnu/libgtk-3.so.0  VlcPlayerSnap-x86_64.AppDir/usr/lib/x86_64-linux-gnu/libgtk-3.so.0.back
 
 wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/vlc.desktop -P Vlc*
 
 wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/vlc.png -P Vlc*
 
+wget https://github.com/cmatomic/RecipesAppimage/raw/master/VLCplayerSnap/VlcSnapAppimage/libunionpreload.so -P Vlc*
+
+#wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/dialog -P Vlc*
+
 wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/vlc.wrapper
 
 wget https://raw.githubusercontent.com/cmatomic/RecipesAppimage/master/VLCplayerSnap/VlcSnapAppimage/vlc.appdata.xml
+
+wget https://github.com/cmatomic/RecipesAppimage/raw/master/VLCplayerSnap/VlcSnapAppimage/libpcre3_8.38-3.1_amd64.tar.gz
+
+wget https://github.com/cmatomic/RecipesAppimage/raw/master/VLCplayerSnap/VlcSnapAppimage/libselinux1_2.4-3build2_amd64.tar.gz
+#( Fedora future suport )
+#wget https://github.com/cmatomic/RecipesAppimage/raw/master/VLCplayerSnap/VlcSnapAppimage/libnsl-2.29-22.fc30.x86_64.tar.gz
 
 chmod +x Vlc*/AppRun
 chmod +x vlc.wrapper
 mv -f vlc.wrapper Vlc*/usr/bin/
 mv -f vlc.appdata.xml Vlc*/usr/share/metainfo/
+tar -xvzf libpcre3_8.38-3.1_amd64.tar.gz -C Vlc*
+tar -xvzf libselinux1_2.4-3build2_amd64.tar.gz -C Vlc*
+#( Fedora future suport )
+#tar -xvzf libnsl-2.29-22.fc30.x86_64.tar.gz -C Vlc*
 
 wget https://github.com/cmatomic/RecipesAppimage/raw/master/VLCplayerSnap/appimagetool
 
